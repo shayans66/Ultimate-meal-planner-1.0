0ae874675b70580b4f1e7e8c8412ee453c95a72e
Shayan Sarnevesht <Shayan@Macintosh-6.local>
2018-02-09 14:50:01 -0800

    connect firebase

--- main.js
+++ main.js
@@ -2,5 +2,10 @@
 
 // Fires off when the document is loading and code is ready to be run!
 $(document).ready(function(){
+
 	// Let the magic begin.
+	firebase.database().ref().set({
+		fact: "Dogs are cool!!!!!1."
+	});
 });
+
