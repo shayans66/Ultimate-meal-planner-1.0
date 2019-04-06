a89aea17a1a5dd49dd2ac616003b0b79009ee5e9
Albert Xiao <albertx7806@gmail.com>
2019-04-06 14:45:01 -0700

    When the user enters a food, request its information from the USDA database

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+API_KEY = 'XUVPTddV3TNR8Bts0DR9JyY7Zftxiv7OgPa8OFsb'
 
-@app.route("/")
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        params = {'api_key': API_KEY, 'q': request.form['food-field'], 'max': 1, 'ds': 'Standard Reference'}
+        r = requests.get('https://api.nal.usda.gov/ndb/search/', params=params)
+        print(r.json())
+        ndbno = r.json()['list']['item'][0]['ndbno']
+
+        params = {'api_key': API_KEY, 'ndbno': [ndbno]}
+        print(params)
+        r = requests.get('https://api.nal.usda.gov/ndb/V2/reports', params=params,
+                         headers={'Content-Type': 'application/json'})
+
+        return render_template('index.html', food=r.json()['foods'][0]['food'])
+
+    return render_template('index.html', food=None)
--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 
-
-// Fires off when the document is loading and code is ready to be run!
-$(document).ready(function(){
-
-	// Let the magic begin.
-	
+$(document).ready(function() {
 	var database = firebase.database();
 	var foodField = document.getElementById('food-field');
 
-	$('#save-button').click(function(){
-		var food = foodField.value;
-		console.log(food);
-		database.ref('food/').push(food).then(function(){
-			console.log('Finished Pushing.')
-		});
-	});
+	// $('#save-button').click(function(){
+	// 	var food = foodField.value;
+	// 	console.log(food);
+	// 	database.ref('food/').push(food).then(function(){
+	// 		console.log('Finished Pushing.')
+	// 	});
+	// });
 });
 
